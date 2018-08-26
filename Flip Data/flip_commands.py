285030bee3c353cc2c1d1cad5495e10e807d3ad8
Amir Shanehsazzadeh <amirshanehsazzadeh@college.harvard.edu>
2018-08-25 23:44:04 -0400

    Update flip_commands.py

--- Flip Data/flip_commands.py
+++ Flip Data/flip_commands.py
@@ -10,7 +10,7 @@ import dihnosir
 
 X = pickle.load(open('flip_dist.pickle', 'rb'))
 Y = pickle.load(open('flip_maxdist.pickle', 'rb'))
-'''
+
 # Compute DIHNOSIR
 strong_dist_clustering = dihnosir.DIHNOSIR(n = 250, silhouette_threshold = 0).fit(X, unmerge = "strong")
 strong_dist_labels = strong_dist_clustering.labels_
@@ -28,7 +28,7 @@ for l_ in strong_maxdist_labels:
     f.write(str(l_))
     f.write(' ')
 f.close()
-'''
+
 weak_dist_clustering = dihnosir.DIHNOSIR(n = 250, minPts_min = 4, silhouette_threshold = 0.6).fit(X, unmerge = "weak")
 weak_dist_labels = weak_dist_clustering.labels_
 f = open('unpruned_weak_flip_dist.txt', 'w')
@@ -36,7 +36,7 @@ for l_ in weak_dist_labels:
     f.write(str(l_))
     f.write(' ')
 f.close()
-'''
+
 weak_maxdist_clustering = dihnosir.DIHNOSIR(n = 250, silhouette_threshold = 0.6).fit(Y, unmerge = "weak")
 weak_maxdist_labels = weak_maxdist_clustering.labels_
 f = open('unpruned_weak_flip_maxdist.txt', 'w')
@@ -44,4 +44,3 @@ for l_ in weak_maxdist_labels:
     f.write(str(l_))
     f.write(' ')
 f.close()
-'''
